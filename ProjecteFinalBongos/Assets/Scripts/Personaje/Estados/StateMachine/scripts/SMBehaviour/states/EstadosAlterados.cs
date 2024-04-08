62242ca7e76199b90f9f32c60c57273ca3a6474f
WBongos <alexga.2003@gmail.com>
2024-04-08 19:08:33 +0200

    Revert "Merge branch 'PersonajeMáquinaEstado'"
    This reverts commit f1cde532b44355b7f6f1c79c93ba166d44b86328, reversing
    changes made to f58ea4c824c9bb7075012f8a7ea340883fb712fd.

--- ProjecteFinalBongos/Assets/Scripts/Personaje/Estados/StateMachine/scripts/SMBehaviour/states/EstadosAlterados.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public enum EstadosAlterados {Invencible, Mullat, Enverinat, Paralitzat, Atordit, Cremat, Ira, Peus_Lleugers, Adormit, Forçut, Atrapat, Normal}
-
- 
-
