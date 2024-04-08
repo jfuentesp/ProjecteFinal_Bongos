6b85804fd75813340a12a8d6f2aa0045c475fcbd
AlexdePablo <adepablos@ies-sabadell.cat>
2024-04-08 03:04:57 +0200

    Tipos de salas
    Scripts de tipos de salas

---
+++ ProjecteFinalBongos/Assets/Scripts/Salas/TiposDeSala/SalaBoss.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SalaBoss : TipoSala
+{
+    [SerializeField]
+    private GameObject m_JefeFinal;
+    public Action OnPLayerInSala;
+
+    protected override void SpawnerSala()
+    {
+        GameObject jefe = Instantiate(m_JefeFinal, transform.position, Quaternion.identity);
+        jefe.transform.parent = transform;
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player"))
+        {
+            return;
+        }
+        else
+        {
+            m_CanOpenDoor = false;
+            cambioPuerta?.Invoke(false);
+            OnPLayerInSala?.Invoke();
+        }
+    }
+
+}
---
+++ ProjecteFinalBongos/Assets/Scripts/Salas/TiposDeSala/TipoSala.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public abstract class TipoSala : MonoBehaviour
+{
+    protected abstract void SpawnerSala();
+    protected bool m_CanOpenDoor;
+    public bool CanOpenDoor => m_CanOpenDoor;
+    public Action<bool> cambioPuerta;
+
+    protected Vector3 GetPosicionSPawnear()
+    {
+        float posRandomX = Random.Range(transform.position.x - 7, transform.position.x + 7);
+        float posRandomY = Random.Range(transform.position.y - 3, transform.position.y + 3);
+
+        Vector3 posicionRandom = new Vector3(posRandomX, posRandomY, 0);
+        return posicionRandom;
+    }
+}
+
