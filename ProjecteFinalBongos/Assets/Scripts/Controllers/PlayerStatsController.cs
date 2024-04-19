0764c62c12ab6df1b74d83a9fd3dca98a13b9926
WBongos <alexga.2003@gmail.com>
2024-04-19 12:14:46 +0200

    Resistencias added

--- ProjecteFinalBongos/Assets/Scripts/Controllers/PlayerStatsController.cs
+++ ProjecteFinalBongos/Assets/Scripts/Controllers/PlayerStatsController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 
 public class PlayerStatsController : MonoBehaviour
@@ -9,16 +10,35 @@ public class PlayerStatsController : MonoBehaviour
     public TimesScriptable m_playerTimes;
     [Header("Modificadores")]
     [SerializeField]
-    public float m_ParalizedLifeModifier;
+    private float m_ParalizedLifeModifier = 2f;
     [SerializeField]
-    public float m_WrathLifeModifier;
-    public float m_WrathSpeedModifier;
-    public float m_WrathStrengthModifier;
-    public float m_WetModifier;
-    public float m_StrengthModifier;
-    public float m_PoisonModifier;
-    public float m_BurntModifier;
-    public float m_VelocityModifier;
+    private float m_WrathLifeModifier = 1.5f;
+    private float m_WrathSpeedModifier;
+    private float m_WrathStrengthModifier;
+    private float m_WetModifier;
+    private float m_StrengthModifier;
+    private float m_PoisonModifier;
+    private float m_BurntModifier;
+    private float m_VelocityModifier;
+    [Header("MaxMin")]
+    private float poisonMin = 5;
+    private float poisonMax = 8;
+    private float burntMin = 10;
+    private float burntMax = 16;
+    private float wrathStrenghtMin = 30;
+    private float wrathStrengthMax = 46;
+    private float wrathSpeedMin = 50;
+    private float wrathSpeedMax = 61;
+    private float wetMin = 10;
+    private float wetMax = 21;
+    private float strengthMin = 20;
+    private float strengthMax = 61;
+    private float speedMin = 50;
+    private float speedMax = 71;
+    [Header("Resistances")]
+    public float m_WaterResistance = 5f;
+    public float m_FireResistance = 5f;
+    public float m_ElectricResistance = 5f;
     [Header("BaseStats")]
     [SerializeField]
     public PlayerBase m_PlayerBaseStats;
@@ -26,13 +46,16 @@ public class PlayerStatsController : MonoBehaviour
     [SerializeField]
     public float m_Velocity;
     [SerializeField]
-    public float m_AttackTime = 1f;
+    public float m_AttackTime;
     [SerializeField]
     public float m_Strength;
     [SerializeField]
     public float m_Defense;
-    public Equipable armadura;
-    public Equipable espada;
+    [SerializeField]
+    private Equipable m_Sword;
+    [SerializeField]
+    private Equipable m_Armor;
+
     private void Start()
     {
         m_Velocity = m_PlayerBaseStats.m_BaseVelocity;
@@ -41,11 +64,38 @@ public class PlayerStatsController : MonoBehaviour
         m_Defense = m_PlayerBaseStats.m_BaseDefense;
     }
 
-    public void EquipArmor() { 
-        
-    }
-    public void EquipSword()
+    public float getModifier(string modifier)
     {
+        switch (modifier)
+        {
+            case "Fast":
+                m_VelocityModifier = Random.Range(speedMin, speedMax);
+                return m_VelocityModifier;
+            case "Wet":
+                m_WetModifier = Random.Range(wetMin, wetMax);
+                return m_WetModifier;
+            case "Burnt":
+                m_BurntModifier = Random.Range(burntMin, burntMax);
+                return m_BurntModifier;
+            case "WrathLife":
+                return m_WrathLifeModifier;
+            case "Paralized":
+                return m_ParalizedLifeModifier;
+            case "WrathStrength":
+                m_WrathStrengthModifier = Random.Range(wrathStrenghtMin, wrathStrengthMax);
+                return m_WrathStrengthModifier;
+            case "WrathSpeed":
+                m_WrathSpeedModifier = Random.Range(wrathSpeedMin, wrathSpeedMax);
+                return m_WrathSpeedModifier;
+            case "Strong":
+                m_StrengthModifier = Random.Range(strengthMin, strengthMax);
+                return m_StrengthModifier;
+            case "Poison":
+                m_PoisonModifier = Random.Range(poisonMin, poisonMax);
+                return m_PoisonModifier;
+            default:
+                return 0;
+        }
 
     }
 }
