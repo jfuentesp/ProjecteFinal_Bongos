967952f2f06fc801ecf8ba991b46fdbacfde6786
Alex <115654119+AlexdePablo@users.noreply.github.com>
2024-04-23 16:28:59 +0200

    Trito

--- ProjecteFinalBongos/Assets/Scripts/Bosses/Water/TritoBossBehaviour.cs
+++ ProjecteFinalBongos/Assets/Scripts/Bosses/Water/TritoBossBehaviour.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(NavMeshAgent))]
 [RequireComponent(typeof(AgentOverride2d))]
@@ -18,27 +19,37 @@ using UnityEngine.AI;
 public class TritoBossBehaviour : BossBehaviour
 {
     [SerializeField] private float m_TiempoEntreSpawn;
+    private Coroutine m_TiempoSpawneoCoroutine;
+    private Coroutine m_DeteccionJugadorCoroutine;
     private new void Awake()
     {
         base.Awake();
         m_SalaPadre = GetComponentInParent<SalaBoss>();
         GetComponent<SMBIdleState>().OnPlayerEnter = (GameObject obj) =>
         {
-            m_StateMachine.ChangeState<TritoArrowSummoningState>();
+            m_StateMachine.ChangeState<SMBChaseState>();
         };
         GetComponent<TritoArrowSummoningState>().onArrowSummoned = () =>
         {
-            m_StateMachine.ChangeState<SMBIdleState>();
+            m_StateMachine.ChangeState<SMBChaseState>();
         };
         GetComponent<TritoArrowSummoningState>().onArrowSummoned += EmpezarCuentaAtras;
+        GetComponent<SMBIdleState>().OnPlayerEnter += EmpezarSpawnFlechas;
         m_StateMachine.ChangeState<SMBIdleState>();
     }
+
+    private void EmpezarSpawnFlechas(GameObject @object)
+    {
+        m_TiempoSpawneoCoroutine = StartCoroutine(TiempoSpawneo());
+    }
+
     private void Start()
     {
     }
     private void EmpezarCuentaAtras()
     {
-        StartCoroutine(TiempoSpawneo());
+        m_DeteccionJugadorCoroutine ??= StartCoroutine(PlayerDetectionCoroutine());
+        m_TiempoSpawneoCoroutine = StartCoroutine(TiempoSpawneo());
     }
 
     private IEnumerator TiempoSpawneo()
@@ -52,6 +63,55 @@ public class TritoBossBehaviour : BossBehaviour
         base.Init(_Target);
         OnPlayerInSala?.Invoke();
     }
+    private IEnumerator PlayerDetectionCoroutine()
+    {
+        while (m_IsAlive)
+        {
+            if (m_PlayerAttackDetectionAreaType == CollisionType.CIRCLE)
+            {
+                RaycastHit2D hitInfo = Physics2D.CircleCast(transform.position, m_AreaRadius, transform.position, m_AreaRadius, m_LayersToCheck);
+                if (hitInfo.collider != null && hitInfo.collider.CompareTag("Player") && !m_IsBusy)
+                {
+                    m_IsPlayerDetected = true;
+                    SetAttack();
+                }
+                else
+                {
+                    m_IsPlayerDetected = false;
+                }
+            }
+            else
+            {
+                RaycastHit2D hitInfo = Physics2D.BoxCast(transform.position, m_BoxArea, transform.rotation.z, transform.position);
+                if (hitInfo.collider.CompareTag("Player") && !m_IsBusy)
+                {
+                    m_IsPlayerDetected = true;
+                    SetAttack();
+                }
+                else
+                {
+                    m_IsPlayerDetected = false;
+                }
+            }
+            yield return new WaitForSeconds(m_CheckingPlayerTimelapse);
+        }
+    }
+    private void SetAttack()
+    {
+        float rng = Random.value;
 
+        switch (rng)
+        {
+            case < 0.5f:
+                m_StateMachine.ChangeState<SMBSingleAttackState>();
+                break;
+            case < 0.65f:
+                m_StateMachine.ChangeState<SMBDoubleAttackState>();
+                break;
+            case > 0.8f:
+                m_StateMachine.ChangeState<SMBTripleAttackState>();
+                break;
+        }
+    }
 
 }
