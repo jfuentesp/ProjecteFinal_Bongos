3d8d4ab26cfd8fc65591c5d3dd5bee66b5d1ad40
AlexdePablo <adepablos@ies-sabadell.cat>
2024-04-23 14:05:10 +0200

    cadenaaaaaaaaaaaaaaaas

--- ProjecteFinalBongos/Assets/Scripts/StateMachine/States/Bosses/Water/Trito/TritoWaterChainsState.cs
+++ ProjecteFinalBongos/Assets/Scripts/StateMachine/States/Bosses/Water/Trito/TritoWaterChainsState.cs
@@ -1,18 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TritoWaterChainsState : SMState
 {
-    // Start is called before the first frame update
-    void Start()
+    public Action onArrowSummoned;
+    [SerializeField]
+    private float m_DurationOfSummoning;
+    private float m_TimeSummoning;
+    private Transform m_Target;
+    private BossBehaviour m_Boss;
+
+    private Pool m_Pool;
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+        m_Boss = GetComponent<BossBehaviour>();
+        m_Pool = LevelManager.Instance._BulletPool;
+        m_Boss.OnPlayerInSala += GetTarget;
+    }
+
+    private void GetTarget()
+    {
+        m_Target = m_Boss.Target;
+    }
+    public override void InitState()
+    {
+        base.InitState();
+        m_TimeSummoning = 0;
+        if (m_Target != null)
+        {
+            GameObject arrow = m_Pool.GetElement();
+            arrow.transform.position = transform.position;
+            arrow.SetActive(true);
+            arrow.GetComponent<TritoChains>().enabled = true;
+            arrow.GetComponent<TritoChains>().Init(m_Target.position - transform.position);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        m_TimeSummoning += Time.deltaTime;
+        if (m_TimeSummoning >= m_DurationOfSummoning)
+        {
+            onArrowSummoned?.Invoke();
+        }
     }
 }
