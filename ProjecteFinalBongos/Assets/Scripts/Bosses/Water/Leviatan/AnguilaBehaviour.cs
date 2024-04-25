f9a99022815f5c0cfb86f566f7683e50a2d1312f
WBongos <alexga.2003@gmail.com>
2024-04-25 20:25:41 +0200

    anguila

---
+++ ProjecteFinalBongos/Assets/Scripts/Bosses/Water/Leviatan/AnguilaBehaviour.cs
@@ -0,0 +1,19 @@
+using NavMeshPlus.Extensions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(AgentOverride2d))]
+[RequireComponent(typeof(SMBIdleState))]
+[RequireComponent(typeof(SMBChaseState))]
+[RequireComponent(typeof(SMBSingleAttackState))]
+[RequireComponent(typeof(HealthController))]
+[RequireComponent(typeof(SMBParriedState))]
+public class AnguilaBehaviour : BossBehaviour
+{ 
+    
+}
+
+
---
+++ ProjecteFinalBongos/Assets/Scripts/Bosses/Water/Leviatan/PiranaBehaviour.cs
@@ -0,0 +1,29 @@
+using NavMeshPlus.Extensions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(AgentOverride2d))]
+[RequireComponent(typeof(SMBIdleState))]
+[RequireComponent(typeof(SMBChaseState))]
+[RequireComponent(typeof(SMBSingleAttackState))]
+[RequireComponent(typeof(SMBDoubleAttackState))]
+[RequireComponent(typeof(SMBTripleAttackState))]
+[RequireComponent(typeof(HealthController))]
+[RequireComponent(typeof(SMBParriedState))]
+public class PiranaBehaviour : BossBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
