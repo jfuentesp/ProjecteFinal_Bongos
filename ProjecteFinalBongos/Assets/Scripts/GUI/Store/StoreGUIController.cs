ccc59c0bc3fad17877c733a80be3ef1dc98ee0e1
AlexCastet <acastetp@ies-sabadell.cat>
2024-05-10 17:54:39 +0200

    Qqqqqqq

---
+++ ProjecteFinalBongos/Assets/Scripts/GUI/Store/StoreGUIController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreGUIController : MonoBehaviour
+{
+    [Header("Store GUI GameObject references")]
+    [SerializeField]
+    private GameObject m_GUIPanel;
+    [Header("Store GUI Components")]
+    [SerializeField]
+    private InventoryController m_PlayerInventory;
+    //[SerializeField]
+    //private Store m_PiccoloStore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
