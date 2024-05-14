9fa71f499204e3a3bbcb749e78ecf31dc89efb50
jfuentesp <117923513+jfuentesp@users.noreply.github.com>
2024-05-14 14:33:40 +0200

    a

--- ProjecteFinalBongos/Assets/Scripts/GUI/Store/StoreGUIController.cs
+++ ProjecteFinalBongos/Assets/Scripts/GUI/Store/StoreGUIController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class StoreGUIController : MonoBehaviour
 {
@@ -10,8 +11,16 @@ public class StoreGUIController : MonoBehaviour
     [Header("Store GUI Components")]
     [SerializeField]
     private InventoryController m_PlayerInventory;
-    //[SerializeField]
-    //private Store m_PiccoloStore;
+    [Header("Arrays that represent each of Piccolo stores")]
+    [SerializeField]
+    private Consumable[] m_PiccoloStoreConsumables = new Consumable[10];
+    [SerializeField]
+    private Equipable[] m_PiccoloStoreEquipables = new Equipable[10];
+    [Header("Lists including all the possible objects that piccolo can show in its store")]
+    [SerializeField]
+    private List<Consumable> m_ConsumableList = new List<Consumable>();
+    [SerializeField]
+    private List<Equipable> m_EquipableList = new List<Equipable>();
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +32,16 @@ public class StoreGUIController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void LoadStore()
+    {
+
+    }
+
+    private void LoadPlayerInventory()
+    {
+
     }
 
     
