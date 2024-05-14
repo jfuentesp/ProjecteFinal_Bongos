4dd2800ea70316e14655ab3d447cf29dee05f118
AlexCastet <acastetp@ies-sabadell.cat>
2024-05-14 20:12:21 +0200

    qsjqj

---
+++ ProjecteFinalBongos/Assets/Scripts/GUI/Store/ShopSlotBehaviour.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ShopSlotBehaviour : MonoBehaviour, ISelectHandler, ISubmitHandler, IDeselectHandler, ICancelHandler, IPointerClickHandler, IPointerEnterHandler
+{
+    [Header("Slot settings")]
+    [SerializeField]
+    private Image m_ItemSprite;
+    [SerializeField]
+    private GameObject m_Slot;
+    [SerializeField]
+    private TextMeshProUGUI m_QuantityText;
+    [SerializeField]
+    private Button m_SlotButton;
+    [SerializeField]
+    private InventoryController m_InventoryController;
+    [SerializeField]
+    private Backpack m_Backpack;
+
+    private Consumable m_AssignedConsumable;
+    public Consumable AssignedConsumable => m_AssignedConsumable;
+
+    private Equipable m_AssignedEquipable;
+    public Equipable AssignedEquipable => m_AssignedEquipable;
+
+    [Header("Action Menu")]
+    [SerializeField]
+    private GameObject m_ActionPanel;
+
+    public void SetConsumable(Consumable consumableToSet)
+    {
+        m_AssignedConsumable = consumableToSet;
+    }
+
+    public void RemoveConsumable()
+    {
+        m_AssignedConsumable = null;
+    }
+
+    public void SetEquipable(Equipable equipableToSet)
+    {
+        m_AssignedEquipable = equipableToSet;
+    }
+
+    public void RemoveEquipable()
+    {
+        m_AssignedEquipable = null;
+    }
+
+    public void RefreshConsumableSlot()
+    {
+        if (m_AssignedConsumable == null)
+        {
+            m_Slot.SetActive(false);
+        }
+        else
+        {
+            m_Slot.SetActive(true);
+            m_ItemSprite.sprite = m_AssignedConsumable.Sprite;
+            m_QuantityText.text = m_Backpack.ConsumableSlots.FirstOrDefault(slot => slot?.Consumable == m_AssignedConsumable).Quantity.ToString();
+        }
+    }
+
+    public void RefreshEquipableSlot()
+    {
+        if (m_AssignedEquipable == null)
+        {
+            m_Slot.SetActive(false);
+        }
+        else
+        {
+            m_Slot.SetActive(true);
+            m_ItemSprite.sprite = m_AssignedEquipable.Sprite;
+            m_QuantityText.text = "";
+        }
+    }
+
+    public void OnCancel(BaseEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        m_InventoryController.SetSelectedItem(gameObject);
+    }
+
+    public void OnSubmit(BaseEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
