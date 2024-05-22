c077d56c4ab5fee5530bee85c4fb58f5e9f1c544
AlexCastet <acastetp@ies-sabadell.cat>
2024-05-22 20:11:04 +0200

    s

--- ProjecteFinalBongos/Assets/Scripts/Items/ScriptableObjects/Consumables/ActivableItem.cs
+++ ProjecteFinalBongos/Assets/Scripts/Items/ScriptableObjects/Consumables/ActivableItem.cs
@@ -14,40 +14,19 @@ public class ActivableItem : Consumable
     private float m_AreaRadius;
     [SerializeField]
     private float m_Duration;
+
+    private Animator m_Animator;
     [SerializeField]
-    private ActivableEnum m_ActivableTypeEnum;
+    private UnityEditor.Animations.AnimatorController m_AnimatorController;
 
     public override void OnUse(GameObject usedBy)
     {
         RaycastHit2D[] enemiesHit = Physics2D.CircleCastAll(usedBy.transform.position, m_AreaRadius, usedBy.transform.position, m_AreaRadius, m_Layermask);
-        if (enemiesHit == null)
-            return;
-        switch (m_ActivableTypeEnum)
+        for(int i = 0; i < enemiesHit.Length; i++)
         {
-            case ActivableEnum.DAMAGE:
-                foreach (RaycastHit2D hitDamage in enemiesHit)
-                {
-                    hitDamage.collider.TryGetComponent(out HealthController healthcontroller);
-                    if(healthcontroller != null)
-                        healthcontroller.Damage(m_DamageAmount);
-                }
-                break;
-            case ActivableEnum.POISON:
-                foreach(RaycastHit2D hitPoison in enemiesHit)
-                {
-                    hitPoison.collider.TryGetComponent(out BossEstadosController bossEstados);
-                    if (bossEstados != null)
-                        bossEstados.AlternarEstado(EstadosAlterados.Enverinat);
-                }
-                break;
-            case ActivableEnum.SLOW:
-                foreach (RaycastHit2D hitSlow in enemiesHit)
-                {
-                    hitSlow.collider.TryGetComponent(out BossEstadosController bossEstados);
-                    if (bossEstados != null)
-                        bossEstados.AlternarEstado(EstadosAlterados.Mullat);
-                }
-                break;
+            enemiesHit[i].collider.TryGetComponent<HealthController>(out HealthController healthcontroller);
+            if (healthcontroller != null)
+                healthcontroller.Damage(m_DamageAmount);
         }
     }
 }
