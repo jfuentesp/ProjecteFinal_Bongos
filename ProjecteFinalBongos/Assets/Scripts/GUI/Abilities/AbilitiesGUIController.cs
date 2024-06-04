8f9ea5b672990f80e00b3cc54b3ee91edc1d8c10
jfuentesp <117923513+jfuentesp@users.noreply.github.com>
2024-06-04 18:09:06 +0200

    arregladas habilidades

--- ProjecteFinalBongos/Assets/Scripts/GUI/Abilities/AbilitiesGUIController.cs
+++ ProjecteFinalBongos/Assets/Scripts/GUI/Abilities/AbilitiesGUIController.cs
@@ -52,7 +52,8 @@ public class AbilitiesGUIController : MonoBehaviour, ISaveableAbilitiesPlayerDat
     private AbilityPointsController m_AbilityPoints;
     public AbilityPointsController AbilityPoints => m_AbilityPoints;
 
-
+    [SerializeField]
+    private GameObject m_ReturnButton;
 
     // Start is called before the first frame update
     void Awake()
